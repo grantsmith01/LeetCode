686c7d02c72558709ed87ab8bac678decd0c5b96
Grant Smith <59898052+grantsmith01@users.noreply.github.com>
2022-09-10 23:34:07 -0700

    Create Square-Root.py

---
+++ Square-Root.py
@@ -0,0 +1,35 @@
+#Coded on AlgoMonster
+
+'''
+Given an integer, find its square root without using the built-in square root function. Only return the integer part (truncate the decimals).
+
+EXAMPLES
+Input: 16
+
+Output: 4
+
+Input: 8
+
+Output: 2
+
+Explanation: square root of 8 is 2.83..., return integer part 2
+'''
+
+def square_root(n: int) -> int:
+    # WRITE YOUR BRILLIANT CODE HERE
+    left = 0
+    right = n
+    sqrt = 0
+    while left <= right:
+        mid = (left + right) // 2
+        if mid**2 <= n:
+            sqrt = mid
+            left = mid + 1
+        else:
+            right = mid - 1
+    return sqrt
+
+if __name__ == '__main__':
+    n = int(input())
+    res = square_root(n)
+    print(res)
